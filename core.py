4940cc2d9b715501c1587797c3a8c38e7f3e6491
Shayan Emami <shayan.emami@hotmail.com>
2020-08-31 16:46:07 +0430

    Keywords

---
+++ core.py
@@ -0,0 +1,13 @@
+keywords = list()
+
+
+
+
+try:
+    while True:
+        key = input("Please Enter Your Loved Keywords ( Cntrl + C To End ) :  ")
+        keywords.append(key)
+except KeyboardInterrupt:
+    pass
+
+print(keywords)
